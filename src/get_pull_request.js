e3f38f001558980daa86919757c984955f569584
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2022-09-08 17:36:18 +0200

    Updated method

--- src/get_pull_request.js
+++ src/get_pull_request.js
@@ -1,51 +1,34 @@
 import axios from "axios";
-import {removeTime, checkBetweenGivenRange,errorMessage} from "./get_pull_request_helper.js"
-function get_pull_requests(owner, repositoryName, startDate, endDate) {
-  const url = `https://api.github.com/repos/${owner}/${repositoryName}/pulls?state=all`;
+import { removeTime, errorMessage } from "./get_pull_request_helper.js";
+function getPullRequests(owner, repositoryName, startDate, endDate) {
+  const url = `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr+created:${startDate}..${endDate}&merged:${startDate}..${endDate}&closed:${startDate}..${endDate}&updated:${startDate}..${endDate}`;
   let outputArray = [];
   axios
     .get(url)
     .then((response) => {
-      for (let i in response.data) {
+      for (let i in response.data.items) {
         let filteredPullRequestData = (({
           id,
           user,
           title,
           state,
           created_at,
-          updated_at,
-          merged_at,
-          closed_at,
         }) => ({
           id,
           user,
           title,
           state,
           created_at,
-          updated_at,
-          merged_at,
-          closed_at,
-        }))(response.data[i]);
+        }))(response.data.items[i]);
         filteredPullRequestData.user = filteredPullRequestData.user.login;
-        startDate = new Date(startDate);
-        endDate = new Date(endDate);
-        filteredPullRequestData.created_at = new Date(
+        filteredPullRequestData.created_at = removeTime(
           filteredPullRequestData.created_at
         );
-        if (checkBetweenGivenRange(filteredPullRequestData,startDate,endDate)) {
-          delete filteredPullRequestData.updated_at;
-          delete filteredPullRequestData.merged_at;
-          delete filteredPullRequestData.closed_at;
-          filteredPullRequestData.created_at = removeTime(
-            filteredPullRequestData.created_at
-          );
-          outputArray.push(filteredPullRequestData);
-        }
+        outputArray.push(filteredPullRequestData);
       }
       console.log(outputArray);
     })
     .catch((err) => {
-      console.log(errorMessage.notFound);
+      throw new Error(errorMessage.notFound);
     });
 }
-
--- src/get_pull_request_helper.js
+++ src/get_pull_request_helper.js
@@ -3,25 +3,5 @@ export const errorMessage = {
 }
 
 export function removeTime(date) {
-  return date.toISOString().slice(0,10);
-}
-export function checkBetweenGivenRange(
-  filteredPullRequestData,
-  startDate,
-  endDate
-) {
-  if (
-    filteredPullRequestData.created_at >= startDate &&
-      filteredPullRequestData.created_at <= endDate ||
-    filteredPullRequestData.updated_at >= startDate &&
-      filteredPullRequestData.updated_at <= endDate ||
-    filteredPullRequestData.closed_at >= startDate &&
-      filteredPullRequestData.closed_at <= endDate ||
-    filteredPullRequestData.merged_at >= startDate &&
-      filteredPullRequestData.merged_at <= endDate
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  return date.slice(0,10);
 }
