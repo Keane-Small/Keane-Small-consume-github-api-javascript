5f9e252bdf12e45bf10cda9209b5c2ee8fb1f467
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2022-10-12 14:49:37 +0200

    Update get_pull_request.js

--- src/get_pull_request.js
+++ src/get_pull_request.js
@@ -1,34 +1,53 @@
 import axios from "axios";
-import { removeTime } from "./get_pull_request_helper.js";
 function removeTime(date) {
-  return date.slice(0, 10);
+  return date.toISOString().slice(0, 10);
+}
+
+function checkBetweenDates(item, startDate, endDate) {
+  const pullRequest = item;
+  pullRequest.created_at = new Date(pullRequest.created_at);
+  pullRequest.updated_at = new Date(pullRequest.updated_at);
+  if (
+    (pullRequest.created_at >= startDate &&
+      pullRequest.created_at <= endDate) ||
+    (pullRequest.updated_at >= startDate && pullRequest.updated_at <= endDate)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getPullRequests(owner, repositoryName, startDate, endDate) {
-  const url = `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr+created:${startDate}..${endDate}&updated:${startDate}..${endDate}`;
+  const url = `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr';`;
   const outputArray = [];
   axios
     .get(url)
     .then((response) => {
-      for (const pr in response.data.items) {
-        const filteredPullRequestData = (({
-          id,
-          user,
-          title,
-          state,
-          created_at,
-        }) => ({
-          id,
-          user,
-          title,
-          state,
-          created_at,
-        }))(response.data.items[pr]);
-        filteredPullRequestData.user = filteredPullRequestData.user.login;
-        filteredPullRequestData.created_at = removeTime(
-          filteredPullRequestData.created_at
-        );
-        outputArray.push(filteredPullRequestData);
+      const allPullRequests = response.data.items;
+      startDate = new Date(startDate);
+      endDate = new Date(endDate);
+      for (const pr in allPullRequests) {
+        if (checkBetweenDates(allPullRequests[pr], startDate, endDate)) {
+          const filteredPullRequestData = (({
+            id,
+            user,
+            title,
+            state,
+            created_at,
+          }) => ({
+            id,
+            user,
+            title,
+            state,
+            created_at,
+          }))(allPullRequests[pr]);
+          filteredPullRequestData.user = filteredPullRequestData.user.login;
+          filteredPullRequestData.created_at = removeTime(
+            filteredPullRequestData.created_at
+          );
+          outputArray.push(filteredPullRequestData);
+        }
       }
       console.log(outputArray);
     })
@@ -38,4 +57,3 @@ function getPullRequests(owner, repositoryName, startDate, endDate) {
       );
     });
 }
-
