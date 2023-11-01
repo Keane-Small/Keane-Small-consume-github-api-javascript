8f43739656c368c5fea5c8abd7def819b996979d
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2023-11-01 20:49:43 +0200

    updated

--- spec/get_pull_requests_spec.js
+++ spec/get_pull_requests_spec.js
@@ -1,22 +1,18 @@
 const MockAdapter = require("axios-mock-adapter");
 const { getPullRequests } = require("../src/get_pull_request");
 const axios = require("axios");
-const spy = require("console-spy");
 
 describe("getPullRequests", () => {
-  let mockedAxios,
-    owner,
-    repositoryName,
-    startDate,
-    endDate,
-    axiosSpy,
-    consoleSpy;
+  let mockedAxios, owner, repositoryName, startDate, endDate, axiosSpy;
+  beforeAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // 10 seconds
+  });
   beforeEach(() => {
     mockedAxios = new MockAdapter(axios);
     owner = "Umuzi-org";
     repositoryName = "ACN-syllabus";
-    startDate = "2022-03-01";
-    endDate = "2022-03-10";
+    startDate = "2023-03-01";
+    endDate = "2023-03-10";
     mockedAxios
       .onGet(
         `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr`
@@ -34,35 +30,81 @@ describe("getPullRequests", () => {
         ],
       });
     axiosSpy = spyOn(axios, "get").and.callThrough();
-    consoleSpy = spy.on(console, "log");
   });
   afterEach(() => {
     mockedAxios.restore();
   });
-  it("should call the api with the right url", async () => {
-    await getPullRequests(owner, repositoryName, startDate, endDate);
+  it("should call the api with the right url", () => {
+    getPullRequests(owner, repositoryName, startDate, endDate);
 
     expect(axiosSpy).toHaveBeenCalledOnceWith(
-      `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr';`
+      `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr;`
     );
   });
   it("should return the correct results from the api call", async () => {
-    const results = await getPullRequests(
+    // Mock the Axios request
+    const owner = "exampleOwner";
+    const repositoryName = "exampleRepo";
+    const startDate = "2023-10-20";
+    const endDate = "2023-10-21";
+
+    const mockData = {
+      items: [
+        {
+          id: 1,
+          user: { login: "user1" },
+          title: "PR 1",
+          state: "open",
+          created_at: "2023-10-20T10:00:00Z",
+          updated_at: "2023-10-20T11:00:00Z",
+        },
+      ],
+    };
+
+    mockedAxios
+      .onGet(
+        `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr`
+      )
+      .reply(200, mockData);
+
+    // Call the function that makes the Axios request
+    const result = await getPullRequests(
       owner,
       repositoryName,
       startDate,
       endDate
     );
 
-    expect(results).toEqual([
+    // Ensure that data is processed correctly
+    expect(result).toEqual([
       {
         id: 1,
-        user: { login: "user1" },
+        user: "user1",
         title: "PR 1",
         state: "open",
-        created_at: "2022-03-05T10:00:00Z",
-        updated_at: "2022-03-05T11:00:00Z",
+        created_at: "2023-10-20",
       },
     ]);
+    // const results = getPullRequests(
+    //   owner,
+    //   repositoryName,
+    //   startDate,
+    //   endDate
+    // ).then((results) => {
+    //   results;
+    //   done();
+    // });
+    // await new Promise((resolve) => setTimeout(resolve, 1000)); // Adjust the timeout as needed
+    // console.log(results);
+    // expect(c).toEqual([
+    //   {
+    //     id: 1,
+    //     user: { login: "user1" },
+    //     title: "PR 1",
+    //     state: "open",
+    //     created_at: "2022-03-05T10:00:00Z",
+    //     updated_at: "2022-03-05T11:00:00Z",
+    //   },
+    // ]);
   });
 });
--- src/get_pull_request.js
+++ src/get_pull_request.js
@@ -20,46 +20,46 @@ function checkBetweenDates(item, startDate, endDate) {
 }
 
 function getPullRequests(owner, repositoryName, startDate, endDate) {
-  const url = `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr';`;
+  const url = `https://api.github.com/search/issues?q=repo:${owner}/${repositoryName}+type:pr;`;
   const outputArray = [];
-  axios
-    .get(url)
-    .then((response) => {
-      const allPullRequests = response.data.items;
-      startDate = new Date(startDate);
-      endDate = new Date(endDate);
-      for (const pr in allPullRequests) {
-        if (checkBetweenDates(allPullRequests[pr], startDate, endDate)) {
-          const filteredPullRequestData = (({
-            id,
-            user,
-            title,
-            state,
-            created_at,
-          }) => ({
-            id,
-            user,
-            title,
-            state,
-            created_at,
-          }))(allPullRequests[pr]);
-          filteredPullRequestData.user = filteredPullRequestData.user.login;
-          filteredPullRequestData.created_at = removeTime(
-            filteredPullRequestData.created_at
-          );
-          outputArray.push(filteredPullRequestData);
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((response) => {
+        const allPullRequests = response.data.items;
+        startDate = new Date(startDate);
+        endDate = new Date(endDate);
+        for (const pr in allPullRequests) {
+          if (checkBetweenDates(allPullRequests[pr], startDate, endDate)) {
+            const filteredPullRequestData = (({
+              id,
+              user,
+              title,
+              state,
+              created_at,
+            }) => ({
+              id,
+              user,
+              title,
+              state,
+              created_at,
+            }))(allPullRequests[pr]);
+            filteredPullRequestData.user = filteredPullRequestData.user.login;
+            filteredPullRequestData.created_at = removeTime(
+              filteredPullRequestData.created_at
+            );
+            outputArray.push(filteredPullRequestData);
+          }
         }
-      }
-      console.log(outputArray);
-      return outputArray;
-    })
-    .catch((err) => {
-      throw new Error(
-        `${err.response.status} User or Repository ${err.response.data.message}`
-      );
-    });
+        console.log(outputArray);
+        resolve(outputArray);
+      })
+      .catch((err) => {
+        reject(
+          `${err.response.status} User or Repository ${err.response.data.message}`
+        );
+      });
+  });
 }
 
-getPullRequests("Umuzi-org", "ACN-syllabus", "2023-10-01", "2023-10-20");
-
-// module.exports = { getPullRequests };
+module.exports = { getPullRequests };
