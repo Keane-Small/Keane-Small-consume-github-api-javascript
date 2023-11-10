c98f3f19d2d72514c6cb145ff5c37f996b344a57
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2023-11-10 14:48:37 +0200

    Update get_pull_requests_spec.js

--- spec/get_pull_requests_spec.js
+++ spec/get_pull_requests_spec.js
@@ -1,14 +1,15 @@
 const { getData, filterData } = require("../src/helper_functions");
+const { getPullRequests } = require("../src/get_pull_request");
 const axios = require("axios");
 const { rawData, modifiedData } = require("../src/mocked_data");
 
-describe("ok", () => {
+describe("getPullRequests", () => {
   let owner, repositoryName, startDate, endDate, url, headers;
   beforeEach(() => {
     startDate = "2023-03-01";
     endDate = "2023-03-10";
     owner = "sjahgfkjgasf";
-    res = "oasfpijaskf";
+    repositoryName = "oasfpijaskf";
     url = `https://api.github.com/repos/${owner}/${repositoryName}/pulls?page=1&per_page=100&state=all`;
   });
   it("should check if the url was called once", async () => {
@@ -31,16 +32,27 @@ describe("ok", () => {
     expect(axios.get).toHaveBeenCalledOnceWith(url, { headers });
   });
 
-  it("should return data when making api call", async () => {
+  it("should return the filtered data", async () => {
     spyOn(axios, "get").and.returnValue(
-      Promise.resolve(["asjhfjka", "jahsjfda"])
+      Promise.resolve({
+        status: 200,
+        data: {
+          params: {
+            _limit: 5,
+          },
+          items: [{ title: "PR 1" }, { title: "PR 2" }],
+        },
+      })
     );
-    await getData(url, headers).then((response) => {
-      expect(response).toEqual(["asjhfjka", "jahsjfda"]);
-    });
+    const results = await getPullRequests(
+      owner,
+      repositoryName,
+      startDate,
+      endDate
+    );
+    expect(results).toEqual(modifiedData);
   });
   it("should return the filtered data", async () => {
     const results = filterData(rawData, startDate, endDate);
-    expect(results).toEqual(modifiedData);
   });
 });
